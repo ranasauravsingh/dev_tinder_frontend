d8b680dde05ca245362dc12a6f674478c882e0a5
ranasauravsingh <ranasaurav121@gmail.com>
2025-05-24 09:37:01 +0530

    [Refactor][Footer] added responsiveness to footer and added redirect portfolio URL

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -1,9 +1,20 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
 	return (
-		<footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4 fixed bottom-0">
+		<footer className="footer footer-center bg-base-300 text-base-content p-4 sm:p-6 fixed bottom-0 w-full">
 			<aside>
-				<p>
-					Made with passion by ranasauravsingh {new Date().getFullYear()}
+				<p className="text-sm sm:text-base">
+					Made with passion by{" "}
+					<Link
+						to="https://ranasauravsingh.site"
+						target="_blank"
+						rel="noopener noreferrer"
+						className="text-info hover:underline"
+					>
+						ranasauravsingh
+					</Link>{" "}
+					{new Date().getFullYear()}
 				</p>
 			</aside>
 		</footer>
