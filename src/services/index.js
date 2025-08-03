cbdb6ae4dbbf900977f96b95decef4b308a2add7
ranasauravsingh <ranasaurav121@gmail.com>
2025-08-03 13:15:31 +0530

    [Refactor][Service] added headers in api route header properties

--- src/services/index.js
+++ src/services/index.js
@@ -1,13 +1,24 @@
 import axios from "axios";
 
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+
 export const POST = async (api_url, data) => {
-	return await axios.post(api_url, data, { withCredentials: true });
+	const headers = {
+		"Content-Type": "application/json",
+	};
+	return await axios.post(api_url, data, { withCredentials: true, headers });
 };
 
 export const GET = async (api_url, data) => {
-	return await axios.get(api_url, { params: data, withCredentials: true });
+	const headers = {
+		"Content-Type": "application/json",
+	};
+	return await axios.get(api_url, { params: data, withCredentials: true, headers });
 };
 
 export const PATCH = async (api_url, data) => {
-	return await axios.patch(api_url, data, { withCredentials: true });
+	const headers = {
+		"Content-Type": "application/json",
+	};
+	return await axios.patch(api_url, data, { withCredentials: true, headers });
 };
