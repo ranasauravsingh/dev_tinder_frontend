8a4ae0a58ba860fc0b56d8f8b9f301de5ff4cf09
ranasauravsingh <ranasaurav121@gmail.com>
2025-08-09 18:52:21 +0530

    [Refactor][Service] added default with credentials to true

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+axios.defaults.withCredentials = true;
 
 export const POST = async (api_url, data) => {
 	const headers = {
